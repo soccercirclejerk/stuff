53982d12da5ca8fea816995c42d1729db9247ff6
soccercirclejerk <soccercirclejerk3497853@mailinator.com>
2013-08-03 19:16:03 +0200

    mobile fix?

--- scripts/deuce.js
+++ scripts/deuce.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
 
+  var mobile = (navigator.userAgent.match(/Android/i) || 
+                navigator.userAgent.match(/webOS/i) ||
+		            navigator.userAgent.match(/iPhone/i) ||
+		            navigator.userAgent.match(/iPod/i) ||
+		            navigator.userAgent.match(/BlackBerry/));
 	var button = $("#button");					   
 	var audio = $('#player audio').get(0);
   
 	button.mousedown(function() {
 	  $(this).addClass("pressed");
-    audio.load();
+    if (mobile) {
+      audio.load();
+    }
 	  audio.play();  
 	});
   
